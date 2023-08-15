acfedf854c93e007b0ce921d6f3a5722dd9f64c7
Stas <postnikov.sa@ya.ru>
2023-08-15 09:22:29 +0700

    Настройка базы данных

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=cg84581_alterra',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
