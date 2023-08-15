bdb662228645c577d61cf5dea517a1d65f152cac
Stas <postnikov.sa@ya.ru>
2023-08-15 14:25:38 +0700

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -2,52 +2,45 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+use yii\helpers\Html;
+
+$this->title = 'Тестовое задание Постников Станислав';
+
 ?>
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
+        <h1 class="display-4 mb-4">Здравствуйте!</h1>
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            <div class="col-md-6">
+                <p class="lead text-justify">Данное тестовое задание успешно выполнено с использованием фреймворка
+                    Yii2. В рамках
+                    задания была создана книга контактов, которая легко доступна по нажатию
+                    кнопки ниже. Она обеспечивает возможность добавления и удаления контактов в унифицированном формате
+                    на базе данных MySQL.</p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="col-md-6">
+                <p class="lead text-justify">
+                    Меня зовут Станислав Постников, мне 36 лет. Я разработчик с опытом работы в
+                    области веб-разработки. Я всегда на связи и готов обсудить возможности
+                    сотрудничества. Вы можете связаться со мной по контактам ниже. Жду интересных предложений и с
+                    нетерпением готов внести вклад в ваш проект!</p>
             </div>
         </div>
 
+        <?= Html::a('Книга Контактов', ['contact/create'], ['class' => 'btn btn-success']) ?>
+
+        <div class="col-md-6 text-left">
+            <h2>Мои контакты</h2>
+            <ul>
+                <li><a href="tel:89101910656">89101910656</a> - телефон</li>
+                <li><a href="https://t.me/postnikovstas">@postnikovstas</a> - телеграм(предпочтительнее)</li>
+                
+                <li><a href="https://hh.ru/resume/2994a2a5ff085fc89e0039ed1f4e554739685a">Мое резюме НН</a></li>
+            </ul>
+        </div>
+
     </div>
+
 </div>
