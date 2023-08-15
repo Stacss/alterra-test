4bdc5516dbc8a0bd114ee0c62f490a3110321c62
Stas <postnikov.sa@ya.ru>
2023-08-15 09:54:19 +0700

    маршруты

--- config/web.php
+++ config/web.php
@@ -43,14 +43,18 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'contact' => 'index',
+                'contact/create' => 'create',
+                'contact/update/<id:\d+>' => 'update',
+                'contact/delete/<id:\d+>' => 'delete',
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
